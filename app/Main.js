25754cc2d056b763e17e282d850e889512a70997
penn <penn201500@gmail.com>
2024-08-16 18:40:42 +0800

    jump to home page if logged in, jump to homeGuest page if logged out

--- app/Main.js
+++ app/Main.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
@@ -6,17 +6,23 @@ import HomeGuest from "./components/HomeGuest"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
+import Home from "./components/Home"
 
 const root = createRoot(document.querySelector("#app"))
 
 function Main() {
+    const [loggedIn, setLoggedIn] = useState(localStorage.getItem("appToken") ? true : false)
+
     return (
         <BrowserRouter>
-            <Header />
+            <Header
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+            />
             <Routes>
                 <Route
                     path="/"
-                    element={<HomeGuest />}
+                    element={loggedIn ? <Home /> : <HomeGuest />}
                 />
                 <Route
                     path="/about-us"
