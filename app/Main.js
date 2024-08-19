2350121620347b0902c0232e885f81b2e4a0756e
penn <penn201500@gmail.com>
2024-08-19 12:15:06 +0800

    use Immer to modify state

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,7 @@
 import React, { useState, useReducer } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useImmerReducer } from "use-immer"
 import Axios from "axios"
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
@@ -23,17 +24,20 @@ function Main() {
         loggedIn: Boolean(localStorage.getItem("appToken")),
         flashMessages: [],
     }
-    function appReducer(state, action) {
+    function appReducer(draft, action) {
         switch (action.type) {
             case "login":
-                return { loggedIn: true, flashMessages: state.flashMessages }
+                draft.loggedIn = true
+                return
             case "logout":
-                return { loggedIn: false, flashMessages: state.flashMessages }
+                draft.loggedIn = false
+                return
             case "flashMessages":
-                return { loggedIn: state.loggedIn, flashMessages: state.flashMessages.concat(action.value) }
+                draft.flashMessages.push(action.value)
+                return
         }
     }
-    const [state, dispatch] = useReducer(appReducer, initialState)
+    const [state, dispatch] = useImmerReducer(appReducer, initialState)
 
     return (
         <StateContext.Provider value={state}>
