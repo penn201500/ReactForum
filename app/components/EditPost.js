273dd4cdffeffe7fba2f830231fbe98be51e6900
penn <penn201500@gmail.com>
2024-08-20 22:31:43 +0800

    fix: if the title and body fields value changed, the popup error flash message should be removed

--- app/components/EditPost.js
+++ app/components/EditPost.js
@@ -35,9 +35,11 @@ function UpdatePost() {
                 draft.isFetching = false
                 return
             case "titleChange":
+                draft.title.hasErrors = false
                 draft.title.value = action.value
                 return
             case "bodyChange":
+                draft.body.hasErrors = false
                 draft.body.value = action.value
                 return
             case "submitRequest":
